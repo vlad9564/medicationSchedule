e17ebd5967cdf1806a7f02e08edfb601fa0ce4ba
Razvan Panait <razvan.panait@cerner.com>
2019-11-13 16:09:51 +0200

    implement profile page

--- src/components/Header/header.index.js
+++ src/components/Header/header.index.js
@@ -1,35 +1,48 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.css";
+import { PagesEnum } from '../../model/PagesEnum.ts';
+
+import CheckIcon from '@material-ui/icons/Check';
 import HealthIcon from "@material-ui/icons/AddToQueue";
+import ClearIcon from '@material-ui/icons/Clear';
 
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-}));
+    render() {
+        if (this.props.page === PagesEnum.MAIN_PAGE) {
+            return (
+                <div className="topBar row" style={{ width: "-webkit-fill-available", position: "fixed", zIndex: "1" }}>
+                    <div className="col-10" style={{ textAlign: "left", paddingLeft: "2.5rem" }}>
+                        <b><label style={{ paddingTop: "1.5rem" }}>MEDICATION SCHEDULE</label></b>
+                    </div>
+                    <div className="col-2" style={{ textAlign: "center" }}>
+                        {/* <Avatar style={{ marginTop: "0.7rem" }} alt="Remy Sharp" src="https://avatars.github.cerner.com/u/8123?s=400&u=46ff427bba820ec8e369b115744c43e9bff2563b" /> */}
+                        <HealthIcon fontSize="large" style={{ margin: "40% 70% 0% 0%" }} />
+                    </div>
+                </div>
+            );
+        }
 
-export default function ButtonAppBar() {
-    const classes = useStyles();
-
-    return (
-        <div className="topBar row" style={{ width: "-webkit-fill-available", position: "fixed", zIndex: "1" }}>
-            <div className="col-10" style={{ textAlign: "left", paddingLeft: "2.5rem" }}>
-                <b><label style={{ paddingTop: "1.5rem" }}>MEDICATION SCHEDULE</label></b>
-            </div>
-            <div className="col-2" style={{ textAlign: "center" }}>
-                {/* <Avatar style={{ marginTop: "0.7rem" }} alt="Remy Sharp" src="https://avatars.github.cerner.com/u/8123?s=400&u=46ff427bba820ec8e369b115744c43e9bff2563b" /> */}
-                <HealthIcon fontSize="large" style={{ margin: "40% 70% 0% 0%" }} />
-            </div>
-        </div>
-    );
+        if (this.props.page === PagesEnum.PROFILE_PAGE) {
+            return (
+                <div className="topBar row" style={{ width: "-webkit-fill-available", position: "fixed", zIndex: "1" }}>
+                    <div className="col-2" style={{ textAlign: "center" }}>
+                        <ClearIcon fontSize="large" style={{ margin: "40% 70% 0% 0%" }} />
+                    </div>
+                    <div className="col-8" style={{ textAlign: "left" }}>
+                        <b><label style={{ paddingTop: "1.5rem" }}>PROFILE</label></b>
+                    </div>
+                    <div className="col-2" style={{ textAlign: "center" }}>
+                        <CheckIcon fontSize="large" style={{ margin: "40% 70% 0% 0%" }} />
+                    </div>
+                </div>
+            );
+        }
+    }
 }
+
+export default Header;
--- src/pages/Profile/profile.index.js
+++ src/pages/Profile/profile.index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "../../components/Header/header.index";
 import BottomNav from '../../components/BottomNav/bottom-nav.index';
+import Avatar from 'react-avatar';
 
 class Profile extends React.Component {
     constructor(props) {
@@ -12,7 +13,11 @@ class Profile extends React.Component {
         console.log(this.props);
         return (
             <div>
-                <Header></Header>
+                <Header page={this.props.page}></Header>
+                <div style={{ marginTop: "4rem", paddingTop: "1rem", paddingLeft: "33%" }}>
+                    {/* <Avatar githubHandle="sitebase" size={150} round="80px" /> */}
+                    <Avatar googleId="118096717852922241760" size="150" round={true} />
+                </div>
                 <BottomNav onPageChange={this.props.onPageChange} page={this.props.page}></BottomNav>
             </div>
         );
